ba25ff67fa669c9738a638d3c9edfeccc4b34f33
DeLaTorreLaraArturo <arturo.delato@hotmail.com>
2017-11-01 10:59:06 -0600

    eliminar contacto del archivo

--- src/Agenda.java
+++ src/Agenda.java
@@ -1,4 +1,10 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.LinkedList;
+import java.util.Scanner;
+import java.util.Iterator;
 
 public class Agenda {
     private LinkedList<Contacto> contactos;//para un acseso secuencial a los elementos
@@ -26,6 +32,61 @@ public class Agenda {
         for(Contacto c : contactos)
         {
             System.out.println(c);
+        }
+    }
+
+    public void guardar(String s) throws IOException {
+        FileWriter archivo = new FileWriter(s);
+        for(Contacto c: contactos)
+        {
+            archivo.write(c.toString());
+            archivo.write("\n");
+        }
+        archivo.close();
+    }
+
+    public void cargar(String nomArchivo) throws FileNotFoundException {
+        Scanner archivo = new Scanner(new File(nomArchivo));
+
+        while(archivo.hasNextLine()){
+            String Linea = archivo.nextLine();
+            //String Linea = archivo.nextLine();
+            String [] arreglo = Linea.split(", ");
+            System.out.println("Nom: "+ arreglo[0]);
+            System.out.println("Dir: "+ arreglo[1]);
+            System.out.println("Tel: "+ arreglo[2]);
+            long tel = Long.parseLong(arreglo[2].trim());
+
+           Contacto nuevo = new Contacto(arreglo[0], arreglo[1], tel);
+           contactos.add(nuevo);
+        }
+    }
+
+    public void eliminarContacto(long telEliminar) {
+    /*    for (Contacto c: contactos) {
+            if (telEliminar == c.dimeTel()){
+                contactos.remove(c);
+                break;
+            }
+
+        }*/
+    //Un iterador es un objto que nos permite acceder a los elementos de una coleccion uno po runo
+        //EL iterador se declara del tipo de dato (objeto) que contiene la coleccion
+        Iterator<Contacto> it;
+
+        //El iterador se inicializa por medio del metodo Iterator() de la coleccion
+        it = contactos.iterator();
+
+        //El iterador tiene  metodos
+        //1. next() - regresa el siguiente elemento d ela coleccion
+        //2. hasNext() - verifica si todavia hya elementos en la coleccion
+        //3. remove() - elimina el elemento actual de la coleccion
+        while (it.hasNext()){
+            Contacto c = it.next();
+            if(telEliminar == c.dimeTel()) {
+                it.remove();
+            }
+            }
         }
     }
 }
